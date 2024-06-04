f9bb4f76ea159bf2f50b8ec814b16b8ff8f9cbca
marceloakalopes <marcelo.lopes2001@hotmail.com>
2024-06-04 13:15:07 -0400

    The <SpeedInsights /> was placed in the wrong place

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -21,8 +21,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="pt-br">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>{children}
       <SpeedInsights />
+      </body>
     </html>
   );
 }
